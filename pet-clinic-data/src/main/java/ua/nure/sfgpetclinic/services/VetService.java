3d964ddd0c2e2fb5c6fd3941906c005f6ba7f9b3
amnangapie <sorosorok@gmail.com>
2020-09-16 23:47:22 +0300

    Implement Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/ua/nure/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package ua.nure.sfgpetclinic.services;
+
+import ua.nure.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
