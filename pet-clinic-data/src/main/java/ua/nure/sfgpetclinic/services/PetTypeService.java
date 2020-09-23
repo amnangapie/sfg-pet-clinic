8e86c0c29bfa1c6c87ba55b84b6cb32c2e122b41
amnangapie <sorosorok@gmail.com>
2020-09-24 00:01:29 +0300

    Adding New Mapservices and Enhance DataLoader

---
+++ pet-clinic-data/src/main/java/ua/nure/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package ua.nure.sfgpetclinic.services;
+
+import ua.nure.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
--- pet-clinic-data/src/main/java/ua/nure/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/ua/nure/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,13 +1,22 @@
 package ua.nure.sfgpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
+import ua.nure.sfgpetclinic.model.Speciality;
 import ua.nure.sfgpetclinic.model.Vet;
+import ua.nure.sfgpetclinic.services.SpecialityService;
 import ua.nure.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,6 +29,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (vet.getSpecialities().size() > 0) {
+            vet.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(vet);
     }
 
