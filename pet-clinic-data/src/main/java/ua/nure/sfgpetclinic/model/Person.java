24bd37eda9270a053bff68804419745aede6c39c
amnangapie <sorosorok@gmail.com>
2020-09-27 14:13:38 +0300

    Lombok Refactoring. Closes #81

--- pet-clinic-data/src/main/java/ua/nure/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ua/nure/sfgpetclinic/model/Person.java
@@ -1,10 +1,25 @@
 package ua.nure.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
+
+    public Person(Long Id, String firstName, String lastName) {
+        super(Id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
     @Column(name = "first_name")
     private String firstName;
@@ -12,17 +27,4 @@ public class Person extends BaseEntity {
     @Column(name = "last_name")
     private String lastName;
 
-    public String getFirstName() { return firstName; }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
